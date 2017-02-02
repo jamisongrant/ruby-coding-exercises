407cce38779ca52c07eb963002359d24218a1e99
jamisongrant <jgbatchelor@gmail.com>
2017-02-02 10:03:11 -0700

    ruby exercises 10-15 january

--- january/14.rb
+++ january/14.rb
@@ -1,5 +1,13 @@
 require 'rspec'
 
+class Array
+  def average  
+    inject(&:+) / size
+  end
+end
+
+# [1, 2, 3].average
+
 describe 'Calculating Average' do
   it 'returns the average value from an array' do
     arr = [100, 50, 75]
