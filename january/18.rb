ee906b960dba8c18adc11ab1ae4fad4aaad08f37
jamisongrant <jgbatchelor@gmail.com>
2017-02-09 10:31:31 -0700

    high / low num game

--- january/18.rb
+++ january/18.rb
@@ -1,3 +1,19 @@
 def game
+  num = rand 25
+  puts "Guess a number between 0 and 24"
+
+  loop do
+    user_answer = gets.chomp.to_i
+
+    if user_answer == num
+      puts "Yay, you got it right!"
+      break
+    elsif user_answer > num
+      puts "You guessed too high, try guessing lower - please guess again!"
+    elsif user_answer < num
+      puts "You guessed too low, try guessing high - please guess again!"
+    end
+  end
 end
 
+game
